d4964b70a4f1f3295ff20ab3086d282e8be60fc2
kcgidw <kc.gideonw@gmail.com>
2020-12-13 18:21:55 -0800

    readme, about

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Genshin Domain Scheduler
+
+Dead-simple domain schedule generator for Genshin Impact.
+
+Work in progress.
+
+Data update/correction PRs very welcome!
+
+## License
+
+GNU General Public License v3.0
--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,28 @@ const About = () => {
 	return (
 		<div id="about">
 			<h1>Genshin Domain Scheduler</h1>
+			<p>GDS is a simple domain schedule generator for Genshin Impact.</p>
+			<p>
+				Looking for more fleshed-out features? Try some of these fellow
+				fansites:
+				<ul className="list-disc list-inside">
+					<li>
+						<a href="https://genshin.poporing.life/">
+							Genshin Farming Calculator
+						</a>
+					</li>
+					<li>
+						<a href="https://genshin-center.com/planner">
+							Genshin Center - Ascension Planner
+						</a>
+					</li>
+					<li>
+						<a href="https://genshin.chiya.dev/">
+							Genshin Schedule
+						</a>
+					</li>
+				</ul>
+			</p>
 			<p>
 				<a href="https://github.com/kcgidw/genshin-domain-scheduler">
 					Github
