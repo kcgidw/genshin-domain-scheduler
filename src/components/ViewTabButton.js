c4d4f94ca55b6015a0f88f2991171b6e26f23e3d
Gideon Wong <gideon.wong@veeva.com>
2020-12-15 17:17:08 -0800

    add underline to active nav tab button

--- src/components/ViewTabButton.js
+++ src/components/ViewTabButton.js
@@ -5,9 +5,11 @@ import { useHover } from 'react-use';
 const ViewTabButton = ({ isActive, onClick, icon, label }) => {
 	const component = (isHovering) => {
 		const cns = cn(
-			'my-1 mx-3 flex items-center space-x-1 text-xs font-mont',
+			'my-1 mx-3 py-1 flex items-center space-x-1 text-xs font-mont border-b-2 border-gray-200 ',
 			{
+				'border-opacity-0': !isActive,
 				'text-gray-200': isHovering || isActive,
+				'border-opacity-40': isActive
 			}
 		);
 		const iconCns = cn({
