336915f3e5c84680d1f1469207d287c33f750dd7
kcgidw <kc.gideonw@gmail.com>
2021-01-15 21:14:29 -0800

    dynamic image import

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,87 +1,16 @@
 import React from 'react';
 import cn from 'classnames';
 
-import amber from '../assets/amber.png';
-import barbara from '../assets/barbara.png';
-import beidou from '../assets/beidou.png';
-import bennett from '../assets/bennett.png';
-import chongyun from '../assets/chongyun.png';
-import diluc from '../assets/diluc.png';
-import fischl from '../assets/fischl.png';
-import kaeya from '../assets/kaeya.png';
-import jean from '../assets/jean.png';
-import keqing from '../assets/keqing.png';
-import klee from '../assets/klee.png';
-import lisa from '../assets/lisa.png';
-import mona from '../assets/mona.png';
-import ningguang from '../assets/ningguang.png';
-import noelle from '../assets/noelle.png';
-import razor from '../assets/razor.png';
-import qiqi from '../assets/qiqi.png';
-import sucrose from '../assets/sucrose.png';
-import venti from '../assets/venti.png';
-import xiangling from '../assets/xiangling.png';
-import xiao from '../assets/xiao.png';
-import xingqiu from '../assets/xingqiu.png';
-import xinyan from '../assets/xinyan.png';
-import zhongli from '../assets/zhongli.png';
-import albedo from '../assets/albedo.png';
-
-import freedom from '../assets/freedom.webp';
-import resistance from '../assets/resistance.webp';
-import ballad from '../assets/ballad.webp';
-import prosperity from '../assets/prosperity.png';
-import diligence from '../assets/diligence.png';
-import gold from '../assets/gold.png';
-
-import boreal from '../assets/boreal.webp';
-import decarabian from '../assets/decarabian.webp';
-import dandelion from '../assets/dandelion gladiator.webp';
-import guyun from '../assets/guyun.webp';
-import elixir from '../assets/elixir.webp';
-import aerosiderite from '../assets/aerosiderite.webp';
-
-const images = {
-	amber,
-	barbara,
-	beidou,
-	bennett,
-	chongyun,
-	diluc,
-	fischl,
-	kaeya,
-	jean,
-	keqing,
-	klee,
-	lisa,
-	mona,
-	ningguang,
-	noelle,
-	qiqi,
-	razor,
-	sucrose,
-	venti,
-	xiangling,
-	xiao,
-	xingqiu,
-	xinyan,
-	zhongli,
-	albedo,
-	// talent
-	freedom,
-	resistance,
-	ballad,
-	prosperity,
-	diligence,
-	gold,
-	// weapon
-	'boreal wolf': boreal,
-	decarabian,
-	'dandelion gladiator': dandelion,
-	elixir,
-	guyun,
-	aerosiderite,
+// Dynamic image import
+const images = {};
+const importAll = (req) => {
+	req.keys().forEach((key) => {
+		// "./abc.png" to "abc"
+		const imageName = key.substring(2).replace(/\..*$/, '');
+		images[imageName] = req(key).default;
+	});
 };
+importAll(require.context('../assets', false, /\.(png|webp)$/));
 
 const CharacterImage = ({ name, shadow }) => {
 	const cns = cn('h-full object-contain inline', {
