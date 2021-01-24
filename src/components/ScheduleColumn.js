7106a5f9e34b27d44b2e5a16e0120e0a93751b8c
kcgidw <kc.gideonw@gmail.com>
2021-01-24 11:27:59 -0800

    fix day-based style

--- src/components/ScheduleColumn.js
+++ src/components/ScheduleColumn.js
@@ -9,14 +9,13 @@ const ScheduleColumn = ({ selectedCharacters, selectedWeapons, day }) => {
 	const cns = cn(
 		'max-w-xs flex-1 flex flex-col section-card border-t border-yellow-200',
 		{
-			'opacity-70 border-opacity-0':
-				todayIdx !== 0 && !isDayPairToday(day),
-			'shadow-lg border-opacity-80':
-				todayIdx === 0 || isDayPairToday(day),
+			'border-opacity-0': !isDayPairToday(day),
+			'opacity-70': todayIdx !== 0 && !isDayPairToday(day),
+			'shadow-lg border-opacity-80': isDayPairToday(day),
 		}
 	);
 	const glowCns = cn({
-		'column-glow': todayIdx === 0 || isDayPairToday(day),
+		'column-glow': isDayPairToday(day),
 	});
 	console.log(isDayPairToday(day));
 	return (
